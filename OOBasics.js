97a7bd474483dbff855747bc37cd61544e1a7806
Hugo <hugo.lima.chaves@gmail.com>
2021-05-21 07:19:45 -0300

    adding a new way of construct a object

--- OOBasics.js
+++ OOBasics.js
@@ -9,9 +9,9 @@ const visualizer = {
 	}
 }
 
-
-
+//Example 1. Using inline creation
 //inline object creation:
+console.log("\nExample 1: ")
 const person = {
 	name: "Hugo",
 	spoken_languages: ["English, Portuguese, Spanish"],
@@ -24,6 +24,7 @@ const person = {
 	}
 }
 
+
 visualizer.warnings("This is the original object created above")
 visualizer.viewObject(person)
 delete person.academic_background // field deletion
@@ -32,5 +33,19 @@ visualizer.viewObject(person)
 person.salute()
 
 
+//Example 2. Using constructor fucntion.
+console.log("\nExample 2: ")
 
+function Product(name, price, description){
+	this.name = name;
+	this.price = price;
+	this.description = description;
+	this.getDiscount = (discount) => {
+		return this.price - discount
+	}
+}
+
+const apple =  new Product("Apple", 1, "Apple: this is not a computer, this one you can eat.");
+visualizer.warnings(apple.description)
+visualizer.viewObject(apple)
 
